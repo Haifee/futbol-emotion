32a98c0c3afd59c2c426d5aa3dd6343722b352c9
Haifee <110411836+Haifee@users.noreply.github.com>
2026-07-25 10:44:42 -0400

    Add files via upload

--- futbol-emotion-laravel/config/tienda.php
+++ futbol-emotion-laravel/config/tienda.php
@@ -23,4 +23,15 @@ return [
     'intentos_maximos'  => (int) env('LOGIN_INTENTOS', 5),
     'bloqueo_segundos'  => (int) env('LOGIN_BLOQUEO_SEG', 300),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Notificaciones push (VAPID)
+    |--------------------------------------------------------------------------
+    | Genera el par de llaves una sola vez y ponlas en las variables de entorno.
+    */
+
+    'vapid_public'  => env('VAPID_PUBLIC_KEY', ''),
+    'vapid_private' => env('VAPID_PRIVATE_KEY', ''),
+    'vapid_subject' => env('VAPID_SUBJECT', 'mailto:admin@futbolemotion.app'),
+
 ];
